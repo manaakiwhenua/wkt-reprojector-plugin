1d7dff6a49f24d3c3ca3826d2f145f0e73d6ea38
Richard Law <richard.m.law@gmail.com>
2020-07-06 09:49:17 +1200

    fixes transformer does not have available_operations property error

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='wkt_reprojector_plugin',
-    version='0.0.2',
+    version='0.0.4',
     description='pygeoapi plugin for performing reprojection on geometries encoded as WKT',
     url='https://github.com/manaakiwhenua/wkt-reprojector-plugin',
     author='Richard Law',
