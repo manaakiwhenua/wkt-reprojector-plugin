7b63045ce7f5ef13a8e9ef71b5fd5b21117ab7fd
Richard Law <richard.m.law@gmail.com>
2020-07-06 12:48:53 +1200

    fix missing import

--- wkt_reprojector_plugin/__init__.py
+++ wkt_reprojector_plugin/__init__.py
@@ -27,6 +27,7 @@
 #
 # =================================================================
 
+import os
 import datetime
 from itertools import repeat
 import logging
@@ -43,7 +44,7 @@ LOGGER = logging.getLogger(__name__)
 CURRENT_YEAR = float(datetime.datetime.now().year)
 
 _USER_PROJ_DATA = os.getenv('PROJ_DIR', None)
-if datadir.get_data_dir() != _USER_PROJ_DATA:
+if _USER_PROJ_DATA and datadir.get_data_dir() != _USER_PROJ_DATA:
     datadir.set_data_dir(_USER_PROJ_DATA)
 
 #: Process metadata and description
